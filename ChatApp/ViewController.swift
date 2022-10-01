bfd4483b80aafa19df15bef729797feaa8a13617
mrwa94 <90597166+mrwa94@users.noreply.github.com>
2022-10-01 18:07:06 +0300

    Initial Commit

---
+++ ChatApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by Ayman alsubhi on 05/03/1444 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
